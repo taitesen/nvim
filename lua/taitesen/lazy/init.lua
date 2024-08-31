96629886159df4e98a3451f6265a737dd0412085
taitesen <iamhmunhlei@gmail.com>
2024-08-31 21:52:12 +0530

    test yugem colorscheme

--- lua/taitesen/lazy/init.lua
+++ lua/taitesen/lazy/init.lua
@@ -6,5 +6,16 @@ return {
     },
 
     "eandrju/cellular-automaton.nvim",
+    {
+        dir = '/home/taitesen/.local/src/config/yugem',
+        lazy = true,
+        priority = 1000,
+        config = function ()
+            require('yugem').setup {}
+        end,
+        init = function ()
+            vim.cmd("colorscheme yugem")
+        end
+    }
 }
 
