d4e39be1b9e36da647bcfbfb30ac4e5c5da818a4
taitesen <iamhmunhlei@gmail.com>
2024-10-06 20:06:30 +0530

    like the code snap more

--- lua/taitesen/lazy/snap.lua
+++ lua/taitesen/lazy/snap.lua
@@ -1,23 +1,20 @@
 return {
-    'krivahtoo/silicon.nvim',
-    build = './install.sh',
-    config = function()
-        require('silicon').setup({
-            font = 'Iosevka Nerd Font Propo=19',
-            theme = 'TwoDark',
-            background = '#87f',
-            line_number = true,
-            pad_vert = 80,
-            pad_horiz = 50,
-            output = {
-                path = "/home/taitesen/Pictures/code"
-            },
-            watermark = {
-                text = ' @taitesen',
-            },
-            window_title = function()
-                return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ':~:.')
-            end,
-        })
-    end
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    opts = {
+        save_path = "~/Pictures/code",
+        has_breadcrumbs = true,
+        bg_theme = "peach", -- bamboo, sea, peach, grape, dusk, summer
+        mac_window_bar = true,
+        title = "Codesnap.nvim",
+        code_font_family = "Iosevka Nerd Font",
+        watermark_font_family = "Pacifico",
+        watermark = "taitesen.nvim",
+        breadcrumbs_separator = "/",
+        has_line_number = false,
+        show_workspace = true,
+        min_width = 0,
+        bg_x_padding = 122,
+        bg_y_padding = 82,
+    },
 }
