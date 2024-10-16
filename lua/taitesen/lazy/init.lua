3f000a97ecc94cf0668da6e9854d36c7e48ef2e6
taitesen <iamhmunhlei@gmail.com>
2024-10-16 23:30:20 +0530

    cleanup files

--- lua/taitesen/lazy/init.lua
+++ lua/taitesen/lazy/init.lua
@@ -6,19 +6,4 @@ return {
     },
 
     "eandrju/cellular-automaton.nvim",
-    {
-        dir = '/home/taitesen/.local/src/config/yugem',
-        lazy = true,
-        priority = 1000,
-        config = function()
-            require('yugem').setup {}
-        end,
-        init = function()
-            -- vim.cmd("colorscheme yugem")
-        end
-    },
-    {
-        'nvim-tree/nvim-web-devicons'
-    }
-
 }
