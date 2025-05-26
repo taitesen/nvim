dadf8f35051be2dd0a29ff075432351a3f1a4441
taitesen <iamhmunhlei@gmail.com>
2025-05-26 22:58:28 +0530

    switch markdown previewr plugin

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,19 +1,8 @@
 return {
-    {
-        "MeanderingProgrammer/markdown.nvim",
-        main = "render-markdown",
-        opts = {},
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
 }
