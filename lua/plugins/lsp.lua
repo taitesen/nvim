d951ae3b0fb67b93e3352bb1794ee807bc24867b
taitesen <iamhmunhlei@gmail.com>
2025-07-10 14:08:52 +0530

    update diagnostic sign

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -127,10 +127,10 @@ return {
             underline = { severity = vim.diagnostic.severity.ERROR },
             signs = {
                 text = {
-                    [vim.diagnostic.severity.ERROR] = "󰅚 ",
-                    [vim.diagnostic.severity.WARN] = "󰀪 ",
-                    [vim.diagnostic.severity.INFO] = "󰋽 ",
-                    [vim.diagnostic.severity.HINT] = "󰌶 ",
+                    [vim.diagnostic.severity.ERROR] = "E",
+                    [vim.diagnostic.severity.WARN] = "W",
+                    [vim.diagnostic.severity.INFO] = "I",
+                    [vim.diagnostic.severity.HINT] = "H",
                 },
             },
             virtual_text = {
